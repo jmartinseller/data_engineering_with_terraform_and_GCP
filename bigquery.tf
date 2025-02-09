5e5d6f41edec33acd9e75a8e4c17b9d6d0df080a
José Willian Martins Eller - JER <jose.eller@iteris.com.br>
2025-02-09 20:17:39 -0300

    Configurações do dataset raw_uncover

--- bigquery.tf
+++ bigquery.tf
@@ -14,43 +14,43 @@ resource "google_bigquery_dataset" "raw_uncover" {
 # Tabelas Externas
 ####################################################################################################
 
-# resource "google_bigquery_table" "tb_leads_sales" {
-#   project = var.data-project
-#   dataset_id = google_bigquery_dataset.raw_uncover.dataset_id
-#   table_id  = "tb_leads_sales"
-#   schema = file("terraform/schemas/tb_leads_sales.json")
+resource "google_bigquery_table" "tb_leads_sales" {
+  project = var.project
+  dataset_id = google_bigquery_dataset.raw_uncover.dataset_id
+  table_id  = "tb_leads_sales"
+  schema = file("terraform/schemas/tb_leads_sales.json")
 
-#   labels = local.labels
+  labels = local.labels
 
-#  external_data_configuration {
-#    autodetect    = true
-#    source_format = "PARQUET" #O formato dos arquivos
-#    source_uris = [
-#      "gs://${var.bk_parquet}/leads_sales.parquet",
-#    ]
-#  }
-# }
+ external_data_configuration {
+   autodetect    = true
+   source_format = "PARQUET" #O formato dos arquivos
+   source_uris = [
+     "gs://${var.bk_parquet}/leads_sales.parquet",
+   ]
+ }
+}
 
-# resource "google_bigquery_table" "tb_tv_radio_influencers" {
-#   project = var.data-project
-#   dataset_id = google_bigquery_dataset.raw_uncover.dataset_id
-#   table_id  = "tb_tv_radio_influencers"
-#   schema = file("terraform/schemas/tb_tv_radio_influencers.json")
+resource "google_bigquery_table" "tb_tv_radio_influencers" {
+  project = var.project
+  dataset_id = google_bigquery_dataset.raw_uncover.dataset_id
+  table_id  = "tb_tv_radio_influencers"
+  schema = file("terraform/schemas/tb_tv_radio_influencers.json")
 
-#   labels = local.labels
+  labels = local.labels
 
-#  external_data_configuration {
-#    autodetect    = true
-#    source_format = "CSV" #O formato dos arquivos
+ external_data_configuration {
+   autodetect    = true
+   source_format = "CSV" #O formato dos arquivos
 
-#    csv_options {
-#      quote                 = "\""
-#      field_delimiter       = ","
-#      skip_leading_rows = 1
-#    }
+   csv_options {
+     quote                 = "\""
+     field_delimiter       = ","
+     skip_leading_rows = 1
+   }
 
-#    source_uris = [
-#      "gs://${var.bk_csv}/*.csv",
-#    ]
-#  }
-# }
+   source_uris = [
+     "gs://${var.bk_csv}/*.csv",
+   ]
+ }
+}
