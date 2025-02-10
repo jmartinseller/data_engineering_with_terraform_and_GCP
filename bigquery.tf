29f4e818587a8e2aea09f467a83e5f9c7c093b07
José Willian Martins Eller - JER <jose.eller@iteris.com.br>
2025-02-10 15:01:59 -0300

    Criação tabela tb_public_test_crm_sms

--- bigquery.tf
+++ bigquery.tf
@@ -74,3 +74,24 @@ resource "google_bigquery_table" "tb_public_test_crm_email" {
     ]
   }
 }
+
+resource "google_bigquery_table" "tb_public_test_crm_sms" {
+  project = var.project
+  dataset_id = google_bigquery_dataset.raw_uncover.dataset_id
+  table_id  = "tb_public_test_crm_sms"
+  schema = file("terraform/schemas/tb_public_test_crm.json")
+  labels = local.labels
+
+  external_data_configuration {
+    autodetect    = true
+    source_format = "GOOGLE_SHEETS"
+
+    google_sheets_options {
+      skip_leading_rows = 1
+    }
+
+    source_uris = [
+      "https://docs.google.com/spreadsheets/d/1a7oLWioF0vzcpuSCbpNjtSvPxyf5HHV5UZCn2f7nQvk/edit?gid=1135476145#gid=1135476145",
+    ]
+  }
+}
