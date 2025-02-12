8c36b03a03d613d989e656d01a7852af997932c4
José Willian Martins Eller - JER <jose.eller@iteris.com.br>
2025-02-11 22:16:20 -0300

    Criação Dataproc

--- dataproc.tf
+++ dataproc.tf
@@ -89,7 +89,7 @@ resource "google_dataproc_workflow_template" "dataproc-jw" {
 # Criando um agendador para rodar o workflow diariamente
 resource "google_cloud_scheduler_job" "daily_trigger" {
   name     = "daily-dataproc-trigger"
-  schedule = "0 3 * * *"  # Executa diariamente às 3h UTC
+  schedule = "0 3 * * *"  # Executa diariamente às 3h UTC 
   time_zone = "America/Sao_Paulo"
 
   http_target {
