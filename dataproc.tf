6a2bfeb80e7d3e080761727a701477bb3b583520
José Willian Martins Eller - JER <jose.eller@iteris.com.br>
2025-02-12 09:16:19 -0300

    auterando subrede

--- dataproc.tf
+++ dataproc.tf
@@ -28,7 +28,7 @@ resource "google_dataproc_workflow_template" "dataproc-jw" {
         software_config {
           image_version = "2.2-debian11"
           properties = {
-            "spark:spark.jars.packages" = "com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.34.0"
+            "spark:spark.jars.packages" = "com.google.cloud.spark:spark-bigquery-with-dependencies_2.12:0.38.0"
           }
         }
 
--- network.tf
+++ network.tf
@@ -9,3 +9,27 @@ resource "google_compute_subnetwork" "subnet_jw" {
   ip_cidr_range = "10.10.0.0/24"
   region        = var.region
 }
+
+resource "google_compute_router" "dataproc_router" {
+  name    = "dataproc-router"
+  network = google_compute_network.vpc_rede_jw.self_link # Altere para a sua VPC, se necessário
+  region  = "us-east1"
+
+  bgp {
+    asn = 64512 # Número padrão para BGP (Border Gateway Protocol)
+  }
+}
+
+resource "google_compute_router_nat" "dataproc_nat" {
+  name   = "dataproc-nat"
+  router = google_compute_router.dataproc_router.name
+  region = google_compute_router.dataproc_router.region
+
+  nat_ip_allocate_option = "AUTO_ONLY" # O GCP aloca os IPs automaticamente
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
