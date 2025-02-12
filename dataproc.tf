84f241d510cc18c4bb32f68ac89bc1e0220be881
José Willian Martins Eller - JER <jose.eller@iteris.com.br>
2025-02-11 23:37:22 -0300

    Criação Dataproc e workflow

--- dataproc.tf
+++ dataproc.tf
@@ -33,7 +33,7 @@ resource "google_dataproc_workflow_template" "dataproc-jw" {
         }
 
         gce_cluster_config {
-          zone             = "us-central1-b"
+          zone             = "us-central1-a"
           internal_ip_only = true
           service_account_scopes = [
             "https://www.googleapis.com/auth/cloud-platform"
