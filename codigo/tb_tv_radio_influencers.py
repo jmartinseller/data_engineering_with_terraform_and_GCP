2e9132b20fd1e510ffa13ad054106c6c563eb78e
José Willian Martins Eller - JER <jose.eller@iteris.com.br>
2025-02-12 17:22:38 -0300

    Fornecendo bucket temporario para o pyspark

--- codigo/tb_tv_radio_influencers.py
+++ codigo/tb_tv_radio_influencers.py
@@ -10,6 +10,9 @@ spark = SparkSession.builder \
 spark.conf.set("spark.sql.catalog.spark_bigquery", "org.apache.spark.sql.execution.datasources.v2.bigquery.BigQueryCatalog")
 spark.conf.set("spark.sql.catalog.spark_bigquery.project", "aprendizado-450314")  # Substitua pelo ID do seu projeto
 
+# Configuração do GCS temporário
+spark.conf.set("spark.hadoop.fs.gs.bucket", "gs://tmp_dataproc_jw")
+
 # Leitura da tabela tb_raw_leads_sales no dataset raw_uncover
 raw_df = spark.read \
     .format("bigquery") \
