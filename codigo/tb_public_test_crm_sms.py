04c29b6a8aef32f3ded54ca74feed975f64d1ee9
José Willian Martins Eller - JER <jose.eller@iteris.com.br>
2025-02-13 09:58:05 -0300

    Corrigindo erro Google Sheet

--- codigo/tb_public_test_crm_sms.py
+++ codigo/tb_public_test_crm_sms.py
@@ -1,6 +1,8 @@
 import gspread
 from pyspark.sql import SparkSession
 from oauth2client.service_account import ServiceAccountCredentials
+from google.cloud import storage
+import os
 
 # Configurações do Google Sheets
 SHEET_URL = 'https://docs.google.com/spreadsheets/d/1a7oLWioF0vzcpuSCbpNjtSvPxyf5HHV5UZCn2f7nQvk/edit?gid=0'
@@ -21,14 +23,21 @@ scope = [
     'https://www.googleapis.com/auth/drive'
 ]
 
-# Carrega as credenciais do arquivo JSON
-creds = ServiceAccountCredentials.from_json_keyfile_name('gs://dependencia_pyspark/key.json', scope)
+# Baixar credenciais do Cloud Storage para um arquivo temporário
+client_storage = storage.Client()
+bucket = client_storage.bucket('dependencia_pyspark')
+blob = bucket.blob('key.json')
+temp_key_path = '/tmp/key.json'
+blob.download_to_filename(temp_key_path)
+
+# Carrega as credenciais do arquivo JSON baixado
+creds = ServiceAccountCredentials.from_json_keyfile_name(temp_key_path, scope)
 
 # Autentica o cliente
-client = gspread.authorize(creds)
+gspread_client = gspread.authorize(creds)
 
 # Abre a planilha pública
-sheet = client.open_by_url(SHEET_URL).worksheet(SHEET_NAME)
+sheet = gspread_client.open_by_url(SHEET_URL).worksheet(SHEET_NAME)
 
 # Lê os dados da planilha
 data = sheet.get_all_values()
@@ -51,6 +60,8 @@ df.write \
     .mode("overwrite") \
     .save()
 
-# Finaliza a SparkSession
+# Removendo o arquivo temporário
+os.remove(temp_key_path)
 
+# Finaliza a SparkSession
 spark.stop()
